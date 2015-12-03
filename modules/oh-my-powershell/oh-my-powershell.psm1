abf39b3b228235abd77c2020a5c2ec5348f24d73
Gonzalo Peci <weedv2@outlook.com>
2015-12-04 12:05:33 +1300

    Improve installation secuence and fix another typo on the profile template

--- modules/oh-my-powershell/oh-my-powershell.psm1
+++ modules/oh-my-powershell/oh-my-powershell.psm1
@@ -3,11 +3,11 @@
 If ( Test-Path ("$Env:USERPROFILE\.powershellrc.ps1") ) {
     Write-Host "Detected a Oh-My-Powershell Profile"
 } else {
-    cp $Env:USERPROFILE"\template\.powershellrc.ps1" $Env:USERPROFILE"\.powershellrc.ps1"
+    cp $Env:USERPROFILE"\.oh-my-powershell\template\powershellrc.ps1" $Env:USERPROFILE"\.powershellrc.ps1"
 }
 
 # Load Profile
 . $Env:USERPROFILE"\.powershellrc.ps1"
 
 # Execute Oh-My-Powershell
-. $Env:USERPROFILE"\.Oh-My-Powershell\oh-my-powershell.ps1"
+. $Env:USERPROFILE"\.oh-my-powershell\oh-my-powershell.ps1"
