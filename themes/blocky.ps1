ef7e3c61b68f82d495ff68e7acfa7056783b87de
Gonzalo Peci <weedv2@outlook.com>
2015-12-03 16:40:30 +1300

    First Theme

---
+++ themes/blocky.ps1
@@ -0,0 +1,18 @@
+function global:prompt {
+    $realCommandStatus = $?
+    $realLASTEXITCODE = $LASTEXITCODE
+    $Host.UI.RawUI.ForegroundColor = $GitPromptSettings.DefaultForegroundColor
+    $Path = $pwd.ProviderPath
+    Write-Host " $Path " -NoNewLine -ForegroundColor Black -BackgroundColor White
+    if($gitStatus){
+        checkGit($Path)
+    }
+    if ( $realCommandStatus -eq $True ) {
+      $BG_EXIT="Green"
+    } else {
+      $BG_EXIT="Red"
+    }
+    $global:LASTEXITCODE = $realLASTEXITCODE
+    Write-Host "`n λ " -NoNewLine -ForegroundColor White -BackgroundColor $BG_EXIT
+    return " "
+}
