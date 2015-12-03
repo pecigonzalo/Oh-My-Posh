4f2c3e84b8d0caef62515aa773482153769006c3
Gonzalo Peci <weedv2@outlook.com>
2015-12-03 16:40:34 +1300

    Add install script and initial module

--- powerprofile.ps1
+++ powerprofile.ps1
@@ -1,6 +1,12 @@
 # Handle Plugins
-
 foreach ($plugin in $plugins) {
-  Write-Host  "C:\Users\GonzaloP\Workspace\PowerProfile\plugins\$plugin\$plugin.ps1"
-  . C:\Users\GonzaloP\Workspace\PowerProfile\plugins\$plugin\$plugin.ps1
+  Write-Host  "C:\Users\GonzaloP\Workspace\Oh-My-Powershell\plugins\$plugin\$plugin.ps1"
+  . "C:\Users\GonzaloP\Workspace\Oh-My-Powershell\plugins\$plugin\$plugin.ps1"
 }
+
+# Load theme
+If ( $theme -eq $null -or $theme -eq "" ) {
+  $theme = "Blocky"
+}
+
+. "C:\Users\GonzaloP\Workspace\Oh-My-Powershell\themes\$theme.ps1"
