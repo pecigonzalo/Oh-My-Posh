1553dd92dbda2bf36d7390c8e98339d34d7079f1
Gonzalo Peci <weedv2@outlook.com>
2015-12-04 10:11:57 +1300

    Install tool to C:\Users\GonzaloP\OneDrive\Datacom\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1/.oh-my-powershell

--- install.ps1
+++ install.ps1
@@ -1,2 +1,6 @@
+# Deploy to home
+rm -Force -Recurse "$env:HOME\.oh-my-powershell" -ErrorAction SilentlyContinue
+cp -Recurse -Force .\  "$env:HOME\.oh-my-powershell"
+
 # Copy module to the user modules folder
 cp -Recurse -Force .\modules\oh-my-powershell  "$([Environment]::GetFolderPath("mydocuments"))\WindowsPowerShell\Modules\"
--- modules/oh-my-powershell/oh-my-powershell.psm1
+++ modules/oh-my-powershell/oh-my-powershell.psm1
@@ -1,11 +1,13 @@
 # Check if profile exists
 
 If ( Test-Path ("$Env:USERPROFILE\.powershellrc.ps1") ) {
-    Write-Host "Mock copy a tempalte profile"
+    Write-Host "Detected a Oh-My-Powershell Profile"
+} else {
+    cp $Env:USERPROFILE"\template\.powershellrc.ps1"
 }
 
 # Load Profile
 . $Env:USERPROFILE"\.powershellrc.ps1"
 
 # Execute Oh-My-Powershell
-. "C:\Users\GonzaloP\Workspace\Oh-My-Powershell\powerprofile.ps1"
+. $Env:USERPROFILE"\.Oh-My-Powershell\oh-my-powershell.ps1"
