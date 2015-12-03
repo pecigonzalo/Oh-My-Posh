a9cea2892cb0b1b358e334762961ed08f8702e4b
Gonzalo Peci <weedv2@outlook.com>
2015-12-04 12:05:33 +1300

    Improve installation secuence and fix another typo on the profile template

--- install.ps1
+++ install.ps1
@@ -1,20 +1,40 @@
 param (
-  [bool]$local = $false
+  [bool]$local = $false,
+  [bool]$force = $false
 )
 
-# Check if Oh-My-Powershell is already installed
-if (Test-Path $Env:USERPROFILE\.oh-my-powershell) {
-  Write-Host "Oh-My-Powershell is already installed"
-} else {
+#
+# Utilities
+#
+function Install-OMP {
   if ($local) {
-      # Deploy from current folder
-      rm -Force -Recurse "$Env:USERPROFILE\.oh-my-powershell" -ErrorAction SilentlyContinue
-      cp -Recurse -Force .\  "$Env:USERPROFILE\.oh-my-powershell"
-    } else {
-      # Clone project
-      git clone https://github.com/pecigonzalo/Oh-My-Powershell.git $Env:USERPROFILE\.oh-my-powershell
-    }
+    # Deploy from current folder
+    Write-Host "Deleting $Env:USERPROFILE\.oh-my-powershell"
+    rm -Force -Recurse "$Env:USERPROFILE\.oh-my-powershell" -ErrorAction SilentlyContinue
+    Write-Host "Coping Oh-My-Powershell to its destination"
+    cp -Recurse -Force .\  "$Env:USERPROFILE\.oh-my-powershell"
+  } else {
+    # Clone project
+    Write-Host "Cloning Oh-My-Powershell from Github"
+    git clone https://github.com/pecigonzalo/Oh-My-Powershell.git $Env:USERPROFILE\.oh-my-powershell
+  }
+# Copy module to the user modules folder
+Write-Host "Installting Oh-My-Powershell Module"
+cp -Recurse -Force $Env:USERPROFILE\.oh-my-powershell\modules\oh-my-powershell  "$([Environment]::GetFolderPath("mydocuments"))\WindowsPowerShell\Modules\"
 }
 
-# Copy module to the user modules folder
-cp -Recurse -Force $Env:USERPROFILE\.oh-my-powershell\modules\oh-my-powershell  "$([Environment]::GetFolderPath("mydocuments"))\WindowsPowerShell\Modules\"
+#
+# Install logic
+#
+
+if ( $force -eq $true ) {
+  Install-OMP
+} else {
+  # Check if Oh-My-Powershell is already installed
+  if ( Test-Path $Env:USERPROFILE\.oh-my-powershell ) {
+    Write-Host "Oh-My-Powershell is already installed"
+  } else {
+    Install-OMP
+  }
+}
+
