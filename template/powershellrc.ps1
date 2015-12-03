7efc397ea8c4759ed9f76ea17b28f4b3df5c5ab7
Gonzalo Peci <weedv2@outlook.com>
2015-12-04 10:11:57 +1300

    Install tool to C:\Users\GonzaloP\OneDrive\Datacom\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1/.oh-my-powershell

---
+++ template/powershellrc.ps1
@@ -0,0 +1,8 @@
+# Which plugins would you like to load? (plugins can be found in ~/.oh-my-powershell/plugins/*)
+# Custom plugins may be added to ~/.oh-my-powershell/custom/plugins/
+# Example format: $plugins= "git", "ls", "math"
+# Add wisely, as too many plugins slow down shell startup.
+$plugins= "git", "ls"
+
+# Theme
+$theme = ""
