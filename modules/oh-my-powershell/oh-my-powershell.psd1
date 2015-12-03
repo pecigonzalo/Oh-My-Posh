4cfd73f23c79d32f8c75d548abafc5d375e799b4
Gonzalo Peci <weedv2@outlook.com>
2015-12-03 17:09:03 +1300

    Fix misc issues

---
+++ modules/oh-my-powershell/oh-my-powershell.psd1
@@ -0,0 +1,14 @@
+@{
+ModuleToProcess = 'oh-my-powershell.psm1'
+ModuleVersion = '0.2'
+GUID = 'ba8bc241-60cb-4b3c-aa41-406fffbab38b'
+Author = 'Gonzalo Peci'
+CompanyName = 'Gonzalo Peci'
+Copyright = 'Copyright (c) 2015 MIT.'
+Description = 'Oh-My-Powershell MAgic'
+PowerShellVersion = '3.0'
+FunctionsToExport = '*'
+CmdletsToExport = '*'
+VariablesToExport = '*'
+AliasesToExport = '*'
+}
--- modules/oh-my-powershell/oh-my-powershell.psm1
+++ modules/oh-my-powershell/oh-my-powershell.psm1
@@ -1,13 +1,11 @@
 # Check if profile exists
 
-if (!$Env:HOME) { $Env:HOME = "$Env:USERPROFILE" }
-
-If ( Test-Path ("$env:HOME\.powershellrc.ps1") ) {
+If ( Test-Path ("$Env:USERPROFILE\.powershellrc.ps1") ) {
     Write-Host "Mock copy a tempalte profile"
 }
 
 # Load Profile
-. $env:HOME"\.powershellrc.ps1"
+. $Env:USERPROFILE"\.powershellrc.ps1"
 
 # Execute Oh-My-Powershell
 . "C:\Users\GonzaloP\Workspace\Oh-My-Powershell\powerprofile.ps1"
