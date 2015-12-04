65ff03238520f7d2cc157005c5c1840775e746d4
Gonzalo Peci <weedv2@outlook.com>
2015-12-04 19:08:10 +1300

    Fix install.ps1 to implement powerhsell best practices using PSScriptAnalyzer

--- install.ps1
+++ install.ps1
@@ -9,18 +9,18 @@ param (
 function Install-OMP {
   if ($local) {
     # Deploy from current folder
-    Write-Host "Deleting $Env:USERPROFILE\.oh-my-powershell"
-    rm -Force -Recurse "$Env:USERPROFILE\.oh-my-powershell" -ErrorAction SilentlyContinue
-    Write-Host "Coping Oh-My-Powershell to its destination"
-    cp -Recurse -Force .\  "$Env:USERPROFILE\.oh-my-powershell"
+    Write-Output "Deleting $Env:USERPROFILE\.oh-my-powershell"
+    Remove-Item -Force -Recurse "$Env:USERPROFILE\.oh-my-powershell" -ErrorAction SilentlyContinue
+    Write-Output "Coping Oh-My-Powershell to its destination"
+    Copy-Item -Recurse -Force .\  "$Env:USERPROFILE\.oh-my-powershell"
   } else {
     # Clone project
-    Write-Host "Cloning Oh-My-Powershell from Github"
+    Write-Output "Cloning Oh-My-Powershell from Github"
     git clone https://github.com/pecigonzalo/Oh-My-Powershell.git $Env:USERPROFILE\.oh-my-powershell
   }
 # Copy module to the user modules folder
-Write-Host "Installting Oh-My-Powershell Module"
-cp -Recurse -Force $Env:USERPROFILE\.oh-my-powershell\modules\oh-my-powershell  "$([Environment]::GetFolderPath("mydocuments"))\WindowsPowerShell\Modules\"
+Write-Output "Installting Oh-My-Powershell Module"
+Copy-Item -Recurse -Force $Env:USERPROFILE\.oh-my-powershell\modules\oh-my-powershell  "$([Environment]::GetFolderPath("mydocuments"))\WindowsPowerShell\Modules\"
 }
 
 #
@@ -32,7 +32,7 @@ if ( $force -eq $true ) {
 } else {
   # Check if Oh-My-Powershell is already installed
   if ( Test-Path $Env:USERPROFILE\.oh-my-powershell ) {
-    Write-Host "Oh-My-Powershell is already installed"
+    Write-Output "Oh-My-Powershell is already installed"
   } else {
     Install-OMP
   }
