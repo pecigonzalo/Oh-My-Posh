c16b836f00a05f00e8361e61b0962bb48dc0235d
Gonzalo Peci <weedv2@outlook.com>
2015-12-16 11:35:28 +1300

    Update pscolor plugin to work with the latest pecigonzalo/pscolor

--- plugins/pscolor/pscolor.ps1
+++ plugins/pscolor/pscolor.ps1
@@ -2,11 +2,11 @@ $global:PSColor = @{
     File = @{
         Default    = @{ Color = 'White' }
         Directory  = @{ Color = 'Green'}
+        Reparse    = @{ Color = 'Magenta'}
         Hidden     = @{ Color = 'DarkGray'; Pattern = '^\.' }
         Code       = @{ Color = 'Magenta'; Pattern = '\.(java|c|cpp|cs|js|css|html|Dockerfile|gradle|
             pp|packergitignore|gitattributes|go|)$' }
         Executable = @{ Color = 'Green'; Pattern = '\.(exe|bat|cmd|py|pl|ps1|psm1|vbs|rb|reg|sh|fsx|)$' }
-        Dll        = @{ Color = 'Darkgreen'; Pattern = '\.(dll|pdb)$' }
         Text       = @{ Color = 'Cyan'; Pattern = '\.(txt|cfg|conf|ini|csv|log|config|
             xml|yml|md|markdown|properties|json|todo)$' }
         Compressed = @{ Color = 'Yellow'; Pattern = '\.(zip|tar|gz|rar|jar|war)$' }
