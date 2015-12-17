b79a41957fe1b84dc03c0cc04e71c9403008b295
Gonzalo Peci <weedv2@outlook.com>
2015-12-18 10:38:48 +1300

    Add simple theme

---
+++ themes/base.ps1
@@ -0,0 +1,11 @@
+﻿function global:prompt {
+    $realLASTEXITCODE = $LASTEXITCODE
+    $Host.UI.RawUI.ForegroundColor = "White"
+    Write-Host $pwd.ProviderPath -NoNewLine -ForegroundColor Green
+    if($gitStatus){
+        checkGit($pwd.ProviderPath)
+    }
+    $global:LASTEXITCODE = $realLASTEXITCODE
+    Write-Host "`nλ" -NoNewLine -ForegroundColor "DarkGray"
+    return " "
+}
