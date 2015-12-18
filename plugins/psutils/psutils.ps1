5dea8369e6f655472fcc3dbc836c828fd963f6a7
Gonzalo Peci <weedv2@outlook.com>
2015-12-18 13:33:45 +1300

    Fix bug with installation of psutils

--- plugins/psutils/psutils.ps1
+++ plugins/psutils/psutils.ps1
@@ -1,11 +1,12 @@
 # Clone psutils if its not present
-if (!(Test-Path ./psutils)) {
-  git clone https://github.com/lukesampson/psutils ./psutils
+if (!(Test-Path $PSScriptRoot/psutils)) {
+  Write-Host "Installing psutils"
+  git clone https://github.com/lukesampson/psutils $PSScriptRoot/psutils
 }
 
 # Create an alias for each file to reference the ps1
-if (Test-Path ./psutils) {
-  $files = Get-ChildItem -Filter *.ps1 .\psutils
+if (Test-Path $PSScriptRoot/psutils) {
+  $files = Get-ChildItem -Filter *.ps1 $PSScriptRoot/psutils
   foreach ($file in $files) {
     $name = [System.IO.Path]::GetFileNameWithoutExtension($file)
     Set-Alias -Name $name -Value $file.FullName -Option AllScope -Scope Global
