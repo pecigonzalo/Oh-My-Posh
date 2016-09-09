4f539df465c20c915a464793cc8d906d5f7c3aad
Gonzalo Peci <weedv2@outlook.com>
2016-09-09 13:03:26 +0200

    Rename config file

--- modules/oh-my-posh/oh-my-posh.psm1
+++ modules/oh-my-posh/oh-my-posh.psm1
@@ -1,13 +1,13 @@
 # Check if profile exists
 
-If ( Test-Path ("$Env:USERPROFILE\.powershellrc.ps1") ) {
+If ( Test-Path ("$Env:USERPROFILE\.posh.config") ) {
     Write-Output "Detected a Oh-My-Posh Profile"
 } else {
-    cp $Env:USERPROFILE"\.oh-my-posh\template\powershellrc.ps1" $Env:USERPROFILE"\.powershellrc.ps1"
+    cp $Env:USERPROFILE"\.oh-my-posh\template\posh.config.ps1" $Env:USERPROFILE"\.posh.config"
 }
 
 # Load Profile var
-. $Env:USERPROFILE"\.powershellrc.ps1"
+. $Env:USERPROFILE"\.posh.config"
 
 # Check for updates
 . $Env:USERPROFILE"\.oh-my-posh\tools\check_for_updates.ps1"
