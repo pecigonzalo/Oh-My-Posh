0d53d29e33dc44144e47638581f05a648dd1f264
Gonzalo Peci <pecigonzalo@users.noreply.github.com>
2020-03-06 16:03:17 +0100

    Allow installing on linux

--- install.ps1
+++ install.ps1
@@ -6,38 +6,47 @@ param (
 # Load utils
 . ./tools/utils.ps1
 
+$INSTALL_PATH = "$HOME/.oh-my-posh"
+if ($IsWindows) {
+  $SEPARATOR = ";"
+} else {
+  $SEPARATOR = ":"
+}
+
+
 function Install-OMP {
   Check-Recommends
   Check-Dependencies
 
-  Write-Output "Deleting $Env:USERPROFILE\.oh-my-posh"
-  Remove-Item -Force -Recurse "$Env:USERPROFILE\.oh-my-posh" -ErrorAction SilentlyContinue
+  Write-Output "Deleting $INSTALL_PATH"
+  Remove-Item -Force -Recurse "$INSTALL_PATH" -ErrorAction SilentlyContinue
   if ($local) {
     # Deploy from current folder
     Write-Output "Coping Oh-My-Posh to its destination"
-    Copy-Item -Recurse -Force .\  "$Env:USERPROFILE\.oh-my-posh\"
-  } else {
+    Copy-Item -Recurse -Force ./  "$INSTALL_PATH"
+  }
+  else {
     # Clone project
     Write-Output "Cloning Oh-My-Posh from Github"
-    git clone https://github.com/pecigonzalo/Oh-My-Posh.git $Env:USERPROFILE\.oh-my-posh
+    git clone https://github.com/pecigonzalo/Oh-My-Posh.git "$INSTALL_PATH"
   }
   # Copy module to the user modules folder
-  Write-Output "Installting Oh-My-Posh Module"
-  New-Item -Type Directory "$([Environment]::GetFolderPath("mydocuments"))\WindowsPowerShell\Modules" -Force | Out-Null
-  Copy-Item -Recurse -Force $Env:USERPROFILE\.oh-my-posh\modules\oh-my-posh  `
-    "$([Environment]::GetFolderPath("mydocuments"))\WindowsPowerShell\Modules\"
+  $MODULES_PATH = "$env:PSModulePath".Split("$SEPARATOR")[0]
+  Write-Output "Installting Oh-My-Posh Module to $MODULES_PATH"
+  New-Item -Type Directory "$MODULES_PATH" -Force | Out-Null
+  Copy-Item -Recurse -Force "$INSTALL_PATH/modules/oh-my-posh" "$MODULES_PATH"
 }
 
 #
 # Install logic
 #
-if ( Test-Path $Env:USERPROFILE\.oh-my-posh ) {
+if ( Test-Path "$INSTALL_PATH" ) {
   Write-Output "Oh-My-Posh is already installed"
   if ( $force -eq $true ) {
     Write-Output "Reinstalling Oh-My-Posh"
     Install-OMP
   }
-} else {
+}
+else {
   Install-OMP
 }
-.$PROFILE
